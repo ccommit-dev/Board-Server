86442b3e40526404891b658be5961de15dcdc46c
ccommit <topojs8@daum.net>
2023-09-28 16:20:58 +0900

    ENH: Spring Boot 3.1.4 버전 업로드 - spring-initializer 를 통해 생성한 프로젝트 업로드

---
+++ src/main/java/com/fastcampus/boardserver/BoardServerApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/boardserver/BoardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.boardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
